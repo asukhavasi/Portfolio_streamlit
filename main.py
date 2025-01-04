5ef4c652f49b10f61ee10dafe9ec8f7d5654d0ef
asukh <sukhavasiabilash@gmail.com>
2025-01-04 01:54:33 -0500

    added Links and Images

--- main.py
+++ main.py
@@ -18,14 +18,21 @@ with col2:
 st.write("<h6>Below you can find some of the apps I have build in python. Feel free to contact me!</h6>", unsafe_allow_html=True)
 
 
-col3, col4 = st.columns(2)
+col3, col4 ,col5= st.columns([1.5,0.5,1.5])
 
 df = pd.read_csv("data/data.csv",sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"data/images/{row["image"]}")
+        st.write("[Source Code](https://www.google.com)")
 
-with col4:
+with col5:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"data/images/{row["image"]}")
+        st.write(f"[Source Code]({row['url']})")
+
